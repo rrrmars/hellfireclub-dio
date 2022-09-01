d91c2bf31e0d794d742f48dfe093ad6621bf6cdc
unknown <rosy.marttins@gmail.com>
2022-09-01 16:23:18 -0300

    commit

---
+++ js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDoU-vYJmvEW3KAfeWQW_KQSqs2-mWW4u0",
+    authDomain: "projeto-teste-c0e4e.firebaseapp.com",
+    projectId: "projeto-teste-c0e4e",
+    storageBucket: "projeto-teste-c0e4e.appspot.com",
+    messagingSenderId: "85026379541",
+    appId: "1:85026379541:web:ff196915416ad63c5afb1c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+export default app 
